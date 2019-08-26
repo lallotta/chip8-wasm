a9aba09a6f15b1ae52d994ff2f640a13468d41d1
Lorenzo Allotta <lorenzo.allotta@gmail.com>
2019-08-26 18:51:09 -0400

    Implement wasm exports

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,82 @@
 pub mod cpu;
 pub mod display;
+pub mod keypad;
+
+use cpu::Cpu;
+use display::Display;
+use keypad::Keypad;
+use wasm_bindgen::prelude::*;
+
+static mut CPU: Cpu = Cpu {
+    memory: [0; 4096],
+    v: [0; 16],
+    i: 0,
+    pc: 0,
+    stack: [0; 16],
+    sp: 0,
+    display: Display {
+        gfx: [0; 2048]
+    },
+    keypad: Keypad {
+        keys: [false; 16]
+    },
+    draw_flag: false,
+    dt: 0,
+    st: 0,
+};
+
+#[wasm_bindgen]
+pub fn emulate_cycle() {
+    unsafe {
+        CPU.emulate_cycle();
+    }
+}
+
+#[wasm_bindgen]
+pub fn reset() {
+    unsafe {
+        CPU.reset();
+    }
+}
+
+#[wasm_bindgen]
+pub fn get_memory() -> *const u8 {
+    unsafe {
+        CPU.memory.as_ptr()
+    }
+}
+
+#[wasm_bindgen]
+pub fn get_display() -> *const u8 {
+    unsafe {
+        CPU.display.gfx.as_ptr()
+    }
+}
+
+#[wasm_bindgen]
+pub fn draw_pending() -> bool {
+    unsafe {
+        CPU.draw_flag
+    }
+}
+
+#[wasm_bindgen]
+pub fn unset_draw_flag() {
+    unsafe {
+        CPU.draw_flag = false;
+    }
+}
+
+#[wasm_bindgen]
+pub fn key_down(key: u8) {
+    unsafe {
+        CPU.keypad.key_down(key);
+    }
+}
+
+#[wasm_bindgen]
+pub fn key_up(key: u8) {
+    unsafe {
+        CPU.keypad.key_up(key);
+    }
+}
